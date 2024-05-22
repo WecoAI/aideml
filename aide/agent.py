545293f4b24619628df8c3fbbea750c0012bbde0
Giulio Starace <giulio.starace@gmail.com>
2024-05-22 17:45:42 +0200

    really drill down the importance of the csv file

--- aide/agent.py
+++ aide/agent.py
@@ -117,14 +117,16 @@ class Agent:
     @property
     def _prompt_impl_guideline(self):
         impl_guideline = [
-            "The code should **implement the proposed solution** and **print the value of the evaluation metric computed on a hold-out validation set**.",
+            "The code should **implement the proposed solution**, **print the value of the evaluation metric computed on a hold-out validation set**,",
+            "**AND MOST IMPORTANTLY SAVE PREDICTIONS ON THE PROVIDED UNLABELED TEST DATA IN A `submission.csv` FILE IN THE ./working DIRECTORY.**",
             "The code should be a single-file python program that is self-contained and can be executed as-is.",
             "No parts of the code should be skipped, don't terminate the before finishing the script.",
             "Your response should only contain a single code block.",
             f"Be aware of the running time of the code, it should complete within {humanize.naturaldelta(self.cfg.exec.timeout)}.",
             'All the provided input data is stored in "./input" directory.',
-            '**If there is test data provided for this task, please save the test predictions in a `submission.csv` file in the "./working" directory as described in the task description** This is extremely important since this file is used for grading/evaluation. DO NOT FORGET THE submission.csv file!',
+            '**You MUST submit predictions on the provided unlabeled test data in a `submission.csv` file** file in the "./working" directory as described in the task description** This is extremely important since this file is used for grading/evaluation. DO NOT FORGET THE submission.csv file!',
             'You can also use the "./working" directory to store any temporary files that your code needs to create.',
+            "REMEMBER THE submission.csv FILE!!!!!!!!!!!!!!!!",
         ]
         if self.acfg.expose_prediction:
             impl_guideline.append(
