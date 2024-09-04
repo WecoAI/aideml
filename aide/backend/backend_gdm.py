721dc9ed1f7fc9a3089e4621f54d280ef25dfe7f
Giulio Starace <giulio.starace@gmail.com>
2024-09-04 18:03:23 +0000

    google deepmind gemini backend

--- aide/backend/backend_gdm.py
+++ aide/backend/backend_gdm.py
@@ -5,26 +5,47 @@ import logging
 
 import google.api_core.exceptions
 import google.generativeai as genai
-from funcy import notnone, once, select_values
+from google.generativeai.generative_models import generation_types
 
-from .utils import FunctionSpec, OutputType, backoff_create, opt_messages_to_list
+from .utils import FunctionSpec, OutputType, backoff_create
 
 logger = logging.getLogger("aide")
 
-_client = None  # type: ignore
+model = None  # type: ignore
+generation_config = None  # type: ignore
 
 GDM_TIMEOUT_EXCEPTIONS = (
     google.api_core.exceptions.RetryError,
     google.api_core.exceptions.TooManyRequests,
     google.api_core.exceptions.ResourceExhausted,
 )
+SAFETY_SETTINGS = [
+    {
+        "category": "HARM_CATEGORY_HARASSMENT",
+        "threshold": "BLOCK_NONE",
+    },
+    {
+        "category": "HARM_CATEGORY_HATE_SPEECH",
+        "threshold": "BLOCK_NONE",
+    },
+    {
+        "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+        "threshold": "BLOCK_NONE",
+    },
+    {
+        "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+        "threshold": "BLOCK_NONE",
+    },
+]
 
 
 @once
-def _setup_gdm_client():
-    # We manually define the client. This is normally defined automatically when calling
-    # the API, but it isn't thread-safe, so we anticipate its creation here
-    raise NotImplementedError("GDM is not implemented yet.")
+def _setup_gdm_client(model_name: str, temperature):
+    global model
+    global generation_config
+
+    model = genai.GenerativeModel(model_name)
+    generation_config = genai.GenerationConfig(temperature=temperature)
 
 
 def query(
@@ -33,4 +54,40 @@ def query(
     func_spec: FunctionSpec | None = None,
     **model_kwargs,
 ) -> tuple[OutputType, float, int, int, dict]:
-    raise NotImplementedError("GDM is not implemented yet.")
+    model = model_kwargs.pop("model")
+    temperature = model_kwargs.pop("temperature", None)
+
+    _setup_gdm_client(model, temperature)
+
+    if func_spec is not None:
+        raise NotImplementedError(
+            "GDM supports function calling but we won't use it for now."
+        )
+
+    # GDM gemini api doesnt support system messages outside of the beta
+    messages = [
+        {"role": "user", "content": message}
+        for message in [system_message, user_message]
+        if message
+    ]
+
+    t0 = time.time()
+    response: generation_types.GenerateContentResponse = backoff_create(
+        model.generate_content,
+        GDM_TIMEOUT_EXCEPTIONS,
+        history=messages,
+        config=generation_config,
+        safety_settings=SAFETY_SETTINGS,
+    )
+    req_time = time.time() - t0
+
+    if response.prompt_feedback.block_reason:
+        output = str(response.prompt_feedback)
+    else:
+        output = response.text
+    in_tokens = response.usage_metadata.prompt_token_count
+    out_tokens = response.usage_metadata.candidates_token_count
+    info = {}  # this isnt used anywhere, but is an expected return value
+
+    # only `output` is actually used by scaffolding
+    return output, req_time, in_tokens, out_tokens, info
